58c926a96ff00c6e72558d4d94ec54742f64fc1a
Ben Gottlieb <ben@standalone.com>
2025-07-25 22:59:55 -0500

    added CKRecordBased and PersistedCKRecord

---
+++ Sources/CloudSeeding/CKRecordBased.swift
@@ -0,0 +1,74 @@
+//
+//  CKRecordBased.swift
+//  CloudSeeding
+//
+//  Created by Ben Gottlieb on 7/25/25.
+//
+
+import Foundation
+import CloudKit
+import SwiftData
+
+public protocol CKRecordBased: AnyObject {
+	var cachedRecordData: Data? { get set }
+	
+	func populateCloudRecord(_ record: CKRecord)
+	
+	var ckRecordName: String { get }
+	var ckRecordID: CKRecord.ID { get }
+	static var ckRecordType: CKRecord.RecordType { get }
+	var ckRecordZoneID: CKRecordZone.ID { get }
+}
+
+
+extension CKRecordBased {
+	public var ckRecordID: CKRecord.ID { CKRecord.ID(recordName: ckRecordName, zoneID: ckRecordZoneID) }
+
+	public func updateCloudRecord(_ record: CKRecord) {
+		populateCloudRecord(record)
+		if let modifiedAt = (self as? any PersistedCKRecord)?.modifiedAt, record[.modifiedAt] != modifiedAt {
+			record[.modifiedAt] = modifiedAt
+		}
+	}
+	
+	public var ckRecord: CKRecord {
+		if let record = lastKnownRecord {
+			updateCloudRecord(record)
+			self.lastKnownRecord = record
+			return record
+		}
+		
+		let record = CKRecord(recordType: Self.ckRecordType, recordID: ckRecordID)
+		updateCloudRecord(record)
+		self.lastKnownRecord = record
+		return record
+	}
+	
+	var rawLastKnownRecord: CKRecord? {
+		guard let data = cachedRecordData else { return nil }
+		guard let unarchiver = try? NSKeyedUnarchiver(forReadingFrom: data) else { return nil }
+		unarchiver.requiresSecureCoding = true
+		return CKRecord(coder: unarchiver)
+	}
+	
+	public var lastKnownRecord: CKRecord? {
+		get {
+			guard let record = rawLastKnownRecord else { return nil }
+			updateCloudRecord(record)
+			return record
+		}
+		
+		set {
+			if let newValue {
+				if let old = rawLastKnownRecord?.modificationDate, let new = newValue.modificationDate, old > new { return }
+				let archiver = NSKeyedArchiver(requiringSecureCoding: true)
+				newValue.encode(with: archiver)
+				self.cachedRecordData = archiver.encodedData
+			} else {
+				self.cachedRecordData = nil
+			}
+			assert(cachedRecordData != nil, "Should never set cachedRecordData to nil.")
+		}
+	}
+}
+
---
+++ Sources/CloudSeeding/PersistedCKRecord.swift
@@ -0,0 +1,68 @@
+//
+//  PersistedCKRecord.swift
+//  CloudSeeding
+//
+//  Created by Ben Gottlieb on 7/25/25.
+//
+
+import CloudKit
+import Suite
+import SwiftData
+
+public enum NewerRecord: String { case localNewer, serverNewer, unknownNewer }
+
+public protocol PersistedCKRecord: CKRecordBased & PersistentModel, PresavablePersistentModel {
+	var modifiedAt: Date { get set }
+	var changeRecordedAt: Date? { get set }
+	var syncEngineID: String { get set }
+	init()
+	func resolveConflict(with cloudRecord: CKRecord, newer: NewerRecord)
+	func load(fromCloud record: CKRecord) -> Bool
+	func presave()
+	func removeFromContext()
+}
+
+public extension PersistedCKRecord {
+	init?(record: CKRecord) {
+		self.init()
+		syncEngineID = record.recordID.recordName
+		self.modifiedAt = record[.modifiedAt] ?? record.modificationDate ?? .now
+		if !load(fromCloud: record) { return nil }
+	}
+
+	func newerRecord(than cloudRecord: CKRecord) -> NewerRecord {
+		guard let cloudModifiedAt = cloudRecord.modificationDate else { return .unknownNewer }
+		if cloudModifiedAt > modifiedAt { return .serverNewer }
+		return .localNewer
+	}
+
+	func clearModifiedAt() {
+		changeRecordedAt = nil
+	}
+	
+	func setModifiedAt() {
+		changeRecordedAt = .now
+		modifiedAt = .now
+	}
+	
+	func presave() { }
+	
+	func reportedSave() {
+		guard let modelContext else {
+			print("Trying to save a record without a model context")
+			return
+		}
+		
+		do {
+			modelContext.presave()
+			try modelContext.save()
+		} catch {
+			print("Failed to save record: \(type(of: self)): \(error.localizedDescription)")
+		}
+	}
+	
+	func removeFromContext() {
+		modelContext?.delete(self)
+	}
+}
+
