823c2642746e03e63ca902cc1590e845d656d729
Ben Gottlieb <ben@standalone.com>
2025-08-15 05:42:00 -0500

    added currentUserID

--- Sources/CloudSeeding/CloudKitInterface.swift
+++ Sources/CloudSeeding/CloudKitInterface.swift
@@ -7,23 +7,27 @@
 
 import Foundation
 import CloudKit
+import Combine
 
 @MainActor @Observable public class CloudKitInterface {
 	public static let instance = CloudKitInterface()
 	
-	public var isAvailable = false
+	public private(set) var isAvailable = false
+	nonisolated static public var currentUserID: CKRecord.ID? { currentUserIDValue.value }
+	nonisolated static let currentUserIDValue = CurrentValueSubject<CKRecord.ID?, Never>(nil)
 	
 	var tokens: [NSObjectProtocol] = []
+	var containerID: String?
 	public var container: CKContainer!
 	
-	init() {
-	}
+	private init() { }
 	
 	public func setup(containerID: String?) {
 		tokens.append(NotificationCenter.default.addObserver(forName: Notification.Name.CKAccountChanged, object: nil, queue: .main) { note in
 			Task { await self.checkAccountStatus() }
 		})
 		
+		self.containerID = containerID
 		container = containerID == nil ? CKContainer.default() : CKContainer(identifier: containerID!)
 		Task { await checkAccountStatus() }
 	}
@@ -46,6 +50,11 @@ import CloudKit
 				isAvailable = false
 			@unknown default:
 				break
+			}
+
+			if isAvailable {
+				Self.currentUserIDValue.value = try await container.userRecordID()
+				print("Signed in as: \(Self.currentUserID?.recordName ?? "??")")
 			}
 		} catch {
 			print("Failed to check CloudKit account status: \(error)")
