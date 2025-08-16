9a3e39d9185aee128c242b055507dd901465f1c9
Ben Gottlieb <ben@standalone.com>
2025-08-16 06:03:26 -0500

    added CKRecord extension

---
+++ Sources/CloudSeeding/CKRecord.swift
@@ -0,0 +1,16 @@
+//
+//  CKRecord.swift
+//  CloudSeeding
+//
+//  Created by Ben Gottlieb on 8/15/25.
+//
+
+import Foundation
+import CloudKit
+
+public extension CKRecord {
+	var createdByRecordID: String? {
+		let name = self.creatorUserRecordID?.recordName
+		return name == CKCurrentUserDefaultName ? CloudKitInterface.currentUserID?.recordName : name
+	}
+}
