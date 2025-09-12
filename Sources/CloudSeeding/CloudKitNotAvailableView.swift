bc357ea34ca15757119bbf756e84dda7483e030c
Ben Gottlieb <ben@standalone.com>
2025-09-12 17:21:19 -0500

    added CloudKitNotAvailableView

---
+++ Sources/CloudSeeding/CloudKitNotAvailableView.swift
@@ -0,0 +1,57 @@
+//
+//  CloudKitNotAvailableView.swift
+//  CloudSeeding
+//
+//  Created by Ben Gottlieb on 9/12/25.
+//
+
+import Suite
+
+extension View {
+	@ViewBuilder public func showingCloudKitAvailability() -> some View {
+		self
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			.overlay {
+				CloudKitNotAvailableView()
+			}
+	}
+}
+
+public struct CloudKitNotAvailableView: View {
+	var cloudKit = CloudKitInterface.instance
+	public init() { }
+	
+	public var body: some View {
+		let status = cloudKit.status
+		
+		if status != .signedIn {
+			notSignedInView
+		}
+	}
+	
+	var notSignedInView: some View {
+		ZStack {
+			Color.black.opacity(0.35)
+				.ignoresSafeArea(edges: .all)
+
+			LinearGradient([1.0, 0.7, 0.25, 0.25, 0.7, 1.0].map { Color.black.opacity($0) }, from: .top, to: .bottom)
+				.ignoresSafeArea(edges: .all)
+				.blur(radius: 20)
+			
+			VStack {
+				Text("You must sign in to iCloud to proceed.")
+					.multilineTextAlignment(.center)
+					.font(.largeTitle)
+					.fontDesign(.rounded)
+					.fontWeight(.bold)
+					.foregroundColor(.white)
+					.padding()
+				
+				Button("Open Settings") {
+					UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+				}
+				.safeGlassButton(prominent: false)
+			}
+		}
+	}
+}
